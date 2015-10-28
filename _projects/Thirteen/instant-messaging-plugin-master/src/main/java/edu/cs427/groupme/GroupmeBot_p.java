250785bd670caa8b5fe5fa536dd7b68576c8c94c
pzhao12 <pzhao12@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-10-28 23:19:18 +0000

    test for autobuilt

--- _projects/Thirteen/instant-messaging-plugin-master/src/main/java/edu/cs427/groupme/GroupmeBot_p.java
+++ _projects/Thirteen/instant-messaging-plugin-master/src/main/java/edu/cs427/groupme/GroupmeBot_p.java
@@ -7,7 +7,7 @@ import java.net.MalformedURLException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
-
+//test
 /**
  * This version is a simple implementation of the groupme bot which can send messages.
  * This version needs json library. We didn't modify the pom.xml probably need to fix the dependency issue.
