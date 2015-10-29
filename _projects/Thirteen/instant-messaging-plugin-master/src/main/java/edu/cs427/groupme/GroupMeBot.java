e9376e10377b902360d309a8253274608848b7c6
pzhao12 <pzhao12@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-10-29 00:07:11 +0000

    org.json

--- _projects/Thirteen/instant-messaging-plugin-master/src/main/java/edu/cs427/groupme/GroupMeBot.java
+++ _projects/Thirteen/instant-messaging-plugin-master/src/main/java/edu/cs427/groupme/GroupMeBot.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import org.json.*;
+import org.json.simple.*;
 import java.net.MalformedURLException;
 import java.net.HttpURLConnection;
 import java.net.URL;
