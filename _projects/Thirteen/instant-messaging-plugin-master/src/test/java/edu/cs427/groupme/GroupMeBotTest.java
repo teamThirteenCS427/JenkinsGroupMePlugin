fbe04543fe192a468b1d32c7bd5835dfcd6ea81b
blessin2 <blessin2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-11-03 22:54:32 +0000

    Renamed GMBotTest to GroupMeBotTest

--- _projects/Thirteen/instant-messaging-plugin-master/src/test/java/edu/cs427/groupme/GroupMeBotTest.java
+++ _projects/Thirteen/instant-messaging-plugin-master/src/test/java/edu/cs427/groupme/GroupMeBotTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class GMBotTest {
+public class GroupMeBotTest {
 
 	@Test
 	public void testResponseCode1() {
