dd8b60573cc71ef52663fa74b81dd7862c1a407c
espaill2 <espaill2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-11-03 21:18:08 +0000

    Added unit testing to GroupMeAPIInterface and restructured the class tested in order to mock the connection

--- _projects/Thirteen/instant-messaging-plugin-master/src/main/java/edu/cs427/groupme/GroupMeAPIInterface.java
+++ _projects/Thirteen/instant-messaging-plugin-master/src/main/java/edu/cs427/groupme/GroupMeAPIInterface.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
@@ -20,13 +21,16 @@ public class GroupMeAPIInterface {
 		this.GROUPME_TOKEN = token;
 		this.GROUPME_ID = id;
 	}
-	
-	public JSONObject GET(String endpoint, String param)
+	/**
+	 * Separated the two mostly for unit testing purposes. This one takes an already formed URL to do the GET request.
+	 * @param url - formed url object onto which the REST connection will be established
+	 * @return jsonObject - returns the parsed response in a JSONObject
+	 */
+	public JSONObject GET(URL url)
 	{
 		JSONObject json = null;
 		try 
 		{
-			URL url = new URL(GROUPME_URL + endpoint + "?token=" + GROUPME_TOKEN + param);
 			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
 			
 			conn.setRequestMethod("GET");
@@ -51,6 +55,24 @@ public class GroupMeAPIInterface {
 		}
 		
 		return json;
+	}
+	
+	/**
+	 * This one you can send in the endpoint and url params and the get request gets made.
+	 * @param endpoint - in other words the path to the specific endpoint in the group me service
+	 * @param param - different get params to append to url
+	 * @return returns JSONObject of response
+	 */
+	public JSONObject GET(String endpoint, String param)
+	{
+		JSONObject json = null;
+		try {
+			json = GET(new URL(GROUPME_URL + endpoint + "?token=" + GROUPME_TOKEN + param));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} finally {
+			return json;
+		}
 	}
 
 	public String getGROUPME_TOKEN() {
