c0de0048040d241cf26c7f8501d8222dfec14f56
fricken2 <fricken2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-11-05 20:45:38 +0000

    pzhao12 fricken2 Ignore Root Project for messaging

--- src/main/java/edu/cs427/groupme/GroupMeBuildListener.java
+++ src/main/java/edu/cs427/groupme/GroupMeBuildListener.java
@@ -54,13 +54,18 @@ public class GroupMeBuildListener extends RunListener<AbstractBuild> {
     public void onStarted(AbstractBuild r, TaskListener listener) {
 		String taskName = r.getProject().getDisplayName();
 		String buildNum = r.getDisplayName();
-		GroupMeIMConnection.registerGroupMeBot();
-		CauseAction cause = r.getAction(CauseAction.class);
-		String causeString = "";
-		if(cause != null)
-			causeString = cause.getShortDescription();
-			
-		GroupMeBot.sendTextMessage(taskName + " - "+ buildNum + " build started." + causeString);
+
+		AbstractProject<?, ?> me = r.getProject();
+		AbstractProject<?, ?> root = r.getRootProject();
+		if(me != root) {//if we aren't the root project
+			GroupMeIMConnection.registerGroupMeBot();
+			CauseAction cause = r.getAction(CauseAction.class);
+			String causeString = "";
+			if(cause != null)
+				causeString = cause.getShortDescription();
+				
+			GroupMeBot.sendTextMessage(taskName + " build " + causeString);
+		}
     }
 
 	/**
