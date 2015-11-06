7acb5f02f43867aeb3eaef95493ced51dd87c503
espaill2 <espaill2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-11-06 19:00:48 +0000

    changed timer for testing

--- src/main/java/edu/cs427/groupme/GroupMeMessagePolling.java
+++ src/main/java/edu/cs427/groupme/GroupMeMessagePolling.java
@@ -55,7 +55,7 @@ public class GroupMeMessagePolling {
 			parseResponse((JSONObject)response.get("response"));
 		else if(responseCode == 304 && cont){
 			try {
-			    Thread.sleep(300000);                 
+			    Thread.sleep(3000);                 
 			} catch(InterruptedException ex) {
 			    Thread.currentThread().interrupt();
 			}
