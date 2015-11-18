ddb67f9523abfb2f93842aac452489ac229b8847
espaill2 <espaill2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-11-18 00:23:51 +0000

    logging messages

--- _projects/Thirteen/trunk/src/main/java/edu/cs427/groupme/GroupMeChat.java
+++ _projects/Thirteen/trunk/src/main/java/edu/cs427/groupme/GroupMeChat.java
@@ -1,12 +1,16 @@
 package edu.cs427.groupme;
 
+import java.util.logging.Logger;
+
 import antlr.StringUtils;
 import hudson.plugins.im.IMChat;
 import hudson.plugins.im.IMException;
 import hudson.plugins.im.IMMessageListener;
+import hudson.plugins.im.bot.Bot;
 
 public class GroupMeChat implements IMChat {
-	
+	private static final Logger LOGGER = Logger.getLogger(GroupMeChat.class.getName());
+
 	public GroupMeChat()
 	{
 		
@@ -14,6 +18,7 @@ public class GroupMeChat implements IMChat {
 	
 	@Override
 	public void sendMessage(String message) throws IMException {
+		LOGGER.warning(message);
 		GroupMeBot.sendTextMessage(message);
 	}
 
