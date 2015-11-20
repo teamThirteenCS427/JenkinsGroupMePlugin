66d39bf9a3423500603a60cc792838780d8786a5
hlee145 <hlee145@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-11-20 22:29:30 +0000

    blessin2 hlee145 made tests for storeddata

--- src/test/java/edu/cs427/groupme/GroupMeStoredDataTest.java
+++ src/test/java/edu/cs427/groupme/GroupMeStoredDataTest.java
@@ -3,20 +3,67 @@ package edu.cs427.groupme;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.Test;
 import java.io.File;
+import java.io.IOException;
 
 public class GroupMeStoredDataTest 
 {
+	@Before
+	public void init()
+	{
+		try{
+			GroupMeStoredData.writeToFile("testStoredData.json");
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
 
 	@Test
 	public void testCreateFile() 
 	{
+		try{
+			GroupMeStoredData.writeToFile("testStoredData2.json");
+		}catch (IOException e){
+			e.printStackTrace();
+		}
 		
+		assertTrue(GroupMeStoredData.dataFileExists("testStoredData2.json"));
+	}
+	
+	@Test
+	public void testNullCheck_nonnullinput()
+	{
+		String ret = GroupMeStoredData.nullCheck("HELLO", "WORLD");
+		assertEquals("HELLO", ret);
 	}
 
+	@Test
+	public void testNullCheck_nullinput()
+	{
+		String ret = GroupMeStoredData.nullCheck(null, "WORLD");
+		assertEquals("WORLD", ret);
+	}
+	
+	@Test
+	public void testFileExists()
+	{
+		boolean ret = GroupMeStoredData.dataFileExists("badfilepath");
+		assertFalse(ret);
+		
+		ret = GroupMeStoredData.dataFileExists("testStoredData.json");
+		assertTrue(ret);
+	}
+	
+	@Test
+	public void testReadFile()
+	{
+		
+	}
 }
