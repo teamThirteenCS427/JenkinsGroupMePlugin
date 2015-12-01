dfddb9ec4f531d83a7af38b5bfcd8db449932c0a
espaill2 <espaill2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-01 23:38:46 +0000

    Added Log to check when it goes to sleep

--- src/main/java/hudson/plugins/im/bot/Bot.java
+++ src/main/java/hudson/plugins/im/bot/Bot.java
@@ -303,6 +303,8 @@ public class Bot implements IMMessageListener {
 	private void executeCommand(final IMMessage msg, final Sender s, final String[] args, final BotCommand command)
 			throws IMException {
 		LOGGER.info("command is the unlock command? " + (command instanceof UnlockCommand));
+		LOGGER.info("sleep? " + (this.sleep));
+
 		if(this.sleep){
 			if(command instanceof UnlockCommand){
 				command.executeCommand(Bot.this, chat, msg, s, args);
