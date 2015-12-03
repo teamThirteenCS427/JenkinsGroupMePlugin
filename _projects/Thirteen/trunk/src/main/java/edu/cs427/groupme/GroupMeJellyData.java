e50e100f668275428480d7f76f1d0998eaf73fd9
hlee145 <hlee145@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-03 23:17:28 +0000

    hlee145 blessin2 adding in functions

--- _projects/Thirteen/trunk/src/main/java/edu/cs427/groupme/GroupMeJellyData.java
+++ _projects/Thirteen/trunk/src/main/java/edu/cs427/groupme/GroupMeJellyData.java
@@ -1,10 +1,23 @@
 import hudson.Extension;
 import org.kohsuke.stapler.QueryParameter;
+import hudson.model.RootAction;
 
 public class GroupMeJellyData {
 
-	public String getGroupMeAccessToken(){
+	public String getGroupMeAccessToken() implements RootAction{
 		return "z";
+	}
+	
+	public String getIconFileName(){
+		return "";
+	}
+	
+	public String getDisplayName(){
+		return "GroupMe Settings";
+	}
+	
+	public String getUrlName(){
+		return "groupme";
 	}
 
 }
