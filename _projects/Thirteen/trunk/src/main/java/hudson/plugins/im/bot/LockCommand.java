8e8eda71315d3b34bc2a7b0094dd5b6ed5000d4a
admathu2 <admathu2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-03 23:26:08 +0000

    added lock time

--- _projects/Thirteen/trunk/src/main/java/hudson/plugins/im/bot/LockCommand.java
+++ _projects/Thirteen/trunk/src/main/java/hudson/plugins/im/bot/LockCommand.java
@@ -3,9 +3,11 @@ package hudson.plugins.im.bot;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Random;
+import java.util.logging.Logger;
 
 import org.apache.commons.lang.StringUtils;
 
+import edu.cs427.groupme.GroupMeMessagePolling;
 import hudson.Extension;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
@@ -18,16 +20,22 @@ import hudson.plugins.im.tools.MessageHelper;
  */
 @Extension
 public class LockCommand extends AbstractTextSendingCommand {
-	 @Override
+		private static final Logger LOGGER = Logger.getLogger(LockCommand.class.getName());
+
+		private static final String SYNTAX = " <job> [time_to_lock_in_seconds]";
+		private static final String HELP = SYNTAX + " - schedule a lock for the specificed number of seconds";
+	
+		@Override
 	    public Collection<String> getCommandNames() {
 	        return Collections.singleton("lock");
-	    }
-
-	    private static final String HELP = "";
-	    
+	    }	    
 
 		@Override
 		protected String getReply(Bot bot, Sender sender, String[] args) {
+			if (args.length >= 1) {
+				String lockTime = args[1];
+				LOGGER.info("lock time is" + lockTime);
+			}
 			String msg = "";
 			if(bot.isSleep()){
 				msg += "I am already asleep...";
