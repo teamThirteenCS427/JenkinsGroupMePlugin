45ca71cb2a3cfc287b8b04623c706a91ad85f14f
zavelev2 <zavelev2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-04 22:52:00 +0000

    zavelev2 pzhao12 formatted GroupMeBotConnection

--- _projects/Thirteen/trunk/src/main/java/edu/cs427/groupme/GroupMeBotConnection.java
+++ _projects/Thirteen/trunk/src/main/java/edu/cs427/groupme/GroupMeBotConnection.java
@@ -5,35 +5,37 @@ import org.json.simple.JSONObject;
 public class GroupMeBotConnection implements IGroupMeBotConnection {
 
 	private GroupMeAPIInterface groupmeApi;
-	
-	public GroupMeBotConnection(String token)
-	{
+
+	public GroupMeBotConnection(String token) {
 		groupmeApi = new GroupMeAPIInterface(token);
 	}
-	
-	
-	/* (non-Javadoc)
-	 * @see edu.cs427.groupme.IGroupMeBotConnection#register(java.lang.String, java.lang.String, java.lang.String)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.cs427.groupme.IGroupMeBotConnection#register(java.lang.String,
+	 * java.lang.String, java.lang.String)
 	 */
 	@Override
-	public JSONObject register(String botName, String groupId, String accessToken)
-	{
-		String body = "{ \"bot\" : " + "{ \"name\" : \""+botName+"\", \"group_id\" : \""+groupId+"\" } }";
+	public JSONObject register(String botName, String groupId, String accessToken) {
+		String body = "{ \"bot\" : " + "{ \"name\" : \"" + botName + "\", \"group_id\" : \"" + groupId + "\" } }";
 		return groupmeApi.POST_BODY("/bots", body);
 	}
-	
+
 	/**
 	 * Send message to the GroupMe chat
-	 * @param botId  bot ID used
-	 * @param message message to send to the chat
+	 * 
+	 * @param botId
+	 *            bot ID used
+	 * @param message
+	 *            message to send to the chat
 	 * @return HTTP response code
 	 */
 	@Override
-	public int sendMessage(String botId, String message)
-	{
+	public int sendMessage(String botId, String message) {
 		if (botId.equals(""))
 			return 0;
-		
+
 		String urlParameters = "bot_id=" + botId + "&text=" + message + "&param3=c";
 		return groupmeApi.POST_PARAMS("/bots/post", urlParameters);
 	}
