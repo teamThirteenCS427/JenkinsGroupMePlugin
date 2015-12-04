a44dfb21fe6e2a5f9fd2da7a4cb9ffe73033c3cf
zavelev2 <zavelev2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-04 22:53:38 +0000

    zavelev2 pzhao12 formatted GroupMeIMConnection

--- src/main/java/edu/cs427/groupme/GroupMeIMConnection.java
+++ src/main/java/edu/cs427/groupme/GroupMeIMConnection.java
@@ -10,95 +10,81 @@ import hudson.plugins.im.IMPresence;
 import hudson.plugins.im.IMPublisherDescriptor;
 import hudson.plugins.im.bot.Bot;
 
-
 /**
  * Implementation of AbsractIMConnection that establishes a connection between
  * GroupMe and the IM plugin
  * 
  * @author blessin2 admathu2 espaill2 zavelev2
  */
-public class GroupMeIMConnection extends AbstractIMConnection 
-{
-	
+public class GroupMeIMConnection extends AbstractIMConnection {
+
 	private GroupMeMessagePolling polling = null;
 	private Bot bot;
 	private final AuthenticationHolder authenticationHolder;
 	private IMChat groupMeChat;
-	
+
 	// An interface containing many important connection variables
 	private IMPublisherDescriptor desc;
-	
+
 	/**
 	 * Constructor to instantiate GroupMe Connection
-	 * @param authenticationHolder  The Authentication of the GroupMeConnection
+	 * 
+	 * @param authenticationHolder
+	 *            The Authentication of the GroupMeConnection
 	 */
 	public GroupMeIMConnection(AuthenticationHolder authenticationHolder) {
-	    this.authenticationHolder = authenticationHolder;
+		this.authenticationHolder = authenticationHolder;
 		registerGroupMeBot();
 		instantiateIMBot();
 		startPolling();
 	}
-	
+
 	/**
-	 * Sets up the class IMBot and makes a new GroupMeChat. The requirements for the plugin to run.
+	 * Sets up the class IMBot and makes a new GroupMeChat. The requirements for
+	 * the plugin to run.
 	 */
-	public void instantiateIMBot()
-	{
+	public void instantiateIMBot() {
 		this.groupMeChat = new GroupMeChat();
-		this.bot = new Bot(this.groupMeChat, 
-						   GroupMeStoredData.getGroupMeBotName(), 
-						   GroupMeStoredData.getGroupMeGroupName(), 
-						   GroupMeStoredData.getBotCommandPrefix(), 
-						   this.authenticationHolder); 
+		this.bot = new Bot(this.groupMeChat, GroupMeStoredData.getGroupMeBotName(),
+				GroupMeStoredData.getGroupMeGroupName(), GroupMeStoredData.getBotCommandPrefix(),
+				this.authenticationHolder);
 	}
-	
+
 	/**
 	 * Tells the polling functionality to begin a new instance.
 	 */
-	public void startPolling()
-	{
-		if(this.polling != null) {
+	public void startPolling() {
+		if (this.polling != null) {
 			close();
 		}
 		this.polling = new GroupMeMessagePolling(new GroupMeAPIInterface(GroupMeStoredData.getGroupMeToken()), bot);
 	}
-	
+
 	/**
 	 * Registers the GroupMeBot so that it can be used by the plugin
 	 */
-	public static void registerGroupMeBot()
-	{
+	public static void registerGroupMeBot() {
 		String storedId = GroupMeStoredData.getGroupMeBotId();
-		if (storedId.equals(""))
-		{
-			if (GroupMeBot.isUnregistered())
-			{
-				GroupMeBot.init(GroupMeStoredData.getGroupMeBotName(), 
-								GroupMeStoredData.getGroupMeToken(), 
-								GroupMeStoredData.getGroupMeGroupId(), 
-								new GroupMeBotConnection(GroupMeStoredData.getGroupMeToken()));
+		if (storedId.equals("")) {
+			if (GroupMeBot.isUnregistered()) {
+				GroupMeBot.init(GroupMeStoredData.getGroupMeBotName(), GroupMeStoredData.getGroupMeToken(),
+						GroupMeStoredData.getGroupMeGroupId(),
+						new GroupMeBotConnection(GroupMeStoredData.getGroupMeToken()));
 				GroupMeBot.register();
 			}
-		}
-		else
-		{
-			GroupMeBot.initWithBotId(storedId,
-								GroupMeStoredData.getGroupMeBotName(), 
-								GroupMeStoredData.getGroupMeToken(), 
-								GroupMeStoredData.getGroupMeGroupId(), 
-								new GroupMeBotConnection(GroupMeStoredData.getGroupMeToken()));
+		} else {
+			GroupMeBot.initWithBotId(storedId, GroupMeStoredData.getGroupMeBotName(),
+					GroupMeStoredData.getGroupMeToken(), GroupMeStoredData.getGroupMeGroupId(),
+					new GroupMeBotConnection(GroupMeStoredData.getGroupMeToken()));
 		}
 	}
-	
+
 	/**
 	 * Registers the GroupMeBot regardless of StoredData
 	 */
-	public static void forceRegisterGroupMeBot()
-	{
-		GroupMeBot.init(GroupMeStoredData.getGroupMeBotName(), 
-						GroupMeStoredData.getGroupMeToken(), 
-						GroupMeStoredData.getGroupMeGroupId(), 
-						new GroupMeBotConnection(GroupMeStoredData.getGroupMeToken()));
+	public static void forceRegisterGroupMeBot() {
+		GroupMeBot.init(GroupMeStoredData.getGroupMeBotName(), GroupMeStoredData.getGroupMeToken(),
+				GroupMeStoredData.getGroupMeGroupId(), new GroupMeBotConnection(GroupMeStoredData.getGroupMeToken()));
 		GroupMeBot.register();
 	}
 
@@ -107,7 +93,7 @@ public class GroupMeIMConnection extends AbstractIMConnection
 	 */
 	@Override
 	public boolean connect() {
-		//spawn thread to run polling
+		// spawn thread to run polling
 		this.polling.init();
 		return isConnected();
 	}
@@ -120,7 +106,6 @@ public class GroupMeIMConnection extends AbstractIMConnection
 		return GroupMeStoredData.getGroupMeGroupName();
 	}
 
-	
 	/**
 	 * {@inheritDoc}
 	 */
@@ -136,7 +121,7 @@ public class GroupMeIMConnection extends AbstractIMConnection
 	public void close() {
 		polling.close();
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -157,7 +142,7 @@ public class GroupMeIMConnection extends AbstractIMConnection
 	 */
 	@Override
 	public void setPresence(IMPresence presence, String statusMessage) throws IMException {
-		//Not needed due to GroupMe being a REST API
+		// Not needed due to GroupMe being a REST API
 	}
 
 	/**
@@ -165,7 +150,7 @@ public class GroupMeIMConnection extends AbstractIMConnection
 	 */
 	@Override
 	public void addConnectionListener(IMConnectionListener listener) {
-		//Not needed due to GroupMe being a REST API
+		// Not needed due to GroupMe being a REST API
 	}
 
 	/**
@@ -173,7 +158,7 @@ public class GroupMeIMConnection extends AbstractIMConnection
 	 */
 	@Override
 	public void removeConnectionListener(IMConnectionListener listener) {
-		//Not needed due to GroupMe being a REST API
+		// Not needed due to GroupMe being a REST API
 	}
 
 }
