0c840e4300ad53723d366504df9403cd5a0607bc
zavelev2 <zavelev2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-04 22:00:00 +0000

    Documents for bot, help, and changes command

--- src/main/java/hudson/plugins/im/bot/ChangesCommand.java
+++ src/main/java/hudson/plugins/im/bot/ChangesCommand.java
@@ -19,146 +19,154 @@ import hudson.scm.ChangeLogSet.Entry;
 
 /**
  * Returns a list of changed files
+ * 
  * @author pzhao12 admathu2
  */
 @Extension
 public class ChangesCommand extends AbstractMultipleJobCommand {
+	/**
+	  * {@inheritDoc}
+	  */
 	@Override
-    public Collection<String> getCommandNames() {
-        return Collections.singleton("changes");
-    }
+	public Collection<String> getCommandNames() {
+		return Collections.singleton("changes");
+	}
 
-    private static final String HELP = "changes <job> <build#> - shows the file changes";
+	private static final String HELP = "changes <job> <build#> - shows the file changes";
 
-    @Override
-    protected CharSequence getMessageForJob(AbstractProject<?, ?> project) {
-        
-    	StringBuilder msg = new StringBuilder(32);
-        msg.append(project.getFullDisplayName());
+	@Override
+	protected CharSequence getMessageForJob(AbstractProject<?, ?> project) {
 
-        AbstractBuild<?, ?> lastBuild = project.getLastBuild();
-	
-        while ((lastBuild != null) && lastBuild.isBuilding()) {
-            lastBuild = lastBuild.getPreviousBuild();
-        }
+		StringBuilder msg = new StringBuilder(32);
+		msg.append(project.getFullDisplayName());
 
-        if (lastBuild != null) {
-        	ChangeLogSet<?> changeSet = lastBuild.getChangeSet();
-        	while(changeSet.isEmptySet()){
+		AbstractBuild<?, ?> lastBuild = project.getLastBuild();
+
+		while ((lastBuild != null) && lastBuild.isBuilding()) {
 			lastBuild = lastBuild.getPreviousBuild();
-			if(lastBuild == null){
-				msg.append("No changes for this project.");
-				return msg;
+		}
+
+		if (lastBuild != null) {
+			ChangeLogSet<?> changeSet = lastBuild.getChangeSet();
+			while (changeSet.isEmptySet()) {
+				lastBuild = lastBuild.getPreviousBuild();
+				if (lastBuild == null) {
+					msg.append("No changes for this project.");
+					return msg;
+				}
+				changeSet = lastBuild.getChangeSet();
 			}
-			changeSet = lastBuild.getChangeSet();
-        	}
-        	msg.append("\nFiles Changed: "+getChangedFilePaths(changeSet));
-          } 
-    	else {
-            msg.append("no finished build yet");
-        }
-        return msg;
-    }
-
-    protected CharSequence getMessageForJobWithBuildNum(AbstractProject<?, ?> project, int buildNumber) {
-    	StringBuilder msg = new StringBuilder(32);
-        msg.append(project.getFullDisplayName());
-
-        AbstractBuild<?, ?> build = project.getBuildByNumber(buildNumber);
-    	if(build == null) {
-    		msg.append(" build # " + buildNumber + " doesn't exist");
-    		return msg;
-    	}
-	
-        if(build.isBuilding()){
-	    	msg.append(" build # " + buildNumber + " is currently building.");
+			msg.append("\nFiles Changed: " + getChangedFilePaths(changeSet));
+		} else {
+			msg.append("no finished build yet");
+		}
 		return msg;
 	}
 
-        	ChangeLogSet<?> changeSet = build.getChangeSet();
-        	while(changeSet.isEmptySet()){
-				msg.append(" No changes for build # " + buildNumber);
-				return msg;
-        	}
-        	msg.append("\nFiles Changed: "+getChangedFilePaths(changeSet));
-        return msg;
-    }
-    
-    @Override
-	public String getReply(Bot bot, Sender sender, String[] args) {
-    	//if parameters are project and buildNumber
-    	int buildNumber = -1;
-    	boolean usingBuildNumber = false;
-    	if(args.length >= 3) {
-    		usingBuildNumber = true;
-    		try {
-    			buildNumber = Integer.parseInt(args[2]);
-    		}
-    		catch (NumberFormatException nfe) {
-    			return "Format is not correct for the build number parameter";
-    		}
-    		args = Arrays.copyOfRange(args, 0, 2); 
-    	}
+	protected CharSequence getMessageForJobWithBuildNum(AbstractProject<?, ?> project, int buildNumber) {
+		StringBuilder msg = new StringBuilder(32);
+		msg.append(project.getFullDisplayName());
 
-        Collection<AbstractProject<?, ?>> projects = new ArrayList<AbstractProject<?, ?>>();
+		AbstractBuild<?, ?> build = project.getBuildByNumber(buildNumber);
+		if (build == null) {
+			msg.append(" build # " + buildNumber + " doesn't exist");
+			return msg;
+		}
 
-        final Pair<Mode, String> pair;
-        try {
-            pair = getProjects(sender, args, projects);
-        } catch (CommandException e) {
-            return getErrorReply(sender, e);
-        }
+		if (build.isBuilding()) {
+			msg.append(" build # " + buildNumber + " is currently building.");
+			return msg;
+		}
 
-        if (!projects.isEmpty()) {
-            StringBuilder msg = new StringBuilder();
-                
-            switch(pair.getHead()) {
-            	case SINGLE : break;
-            	case ALL:
-            		msg.append(getCommandShortName())
-            			.append(" of all projects:\n");
-            		break;
-            	case VIEW:
-            		msg.append(getCommandShortName())
-        				.append(" of projects in view " + pair.getTail() + ":\n");
-            		break;
-            }
-
-            boolean first = true;
-            for (AbstractProject<?, ?> project : projects) {
-                if (!first) {
-                    msg.append("\n");
-                } else {
-                    first = false;
-                }
-
-                if(usingBuildNumber)
-                	msg.append(getMessageForJobWithBuildNum(project, buildNumber));
-                else
-                	msg.append(getMessageForJob(project));
-            }
-            return msg.toString();
-        } else {
-            return sender + ": no job found";
-        }
+		ChangeLogSet<?> changeSet = build.getChangeSet();
+		while (changeSet.isEmptySet()) {
+			msg.append(" No changes for build # " + buildNumber);
+			return msg;
+		}
+		msg.append("\nFiles Changed: " + getChangedFilePaths(changeSet));
+		return msg;
 	}
 
-    @Override
-    protected String getCommandShortName() {
-        return "changes";
-    }
+	/**
+	  * {@inheritDoc}
+	  */
+	@Override
+	public String getReply(Bot bot, Sender sender, String[] args) {
+		// if parameters are project and buildNumber
+		int buildNumber = -1;
+		boolean usingBuildNumber = false;
+		if (args.length >= 3) {
+			usingBuildNumber = true;
+			try {
+				buildNumber = Integer.parseInt(args[2]);
+			} catch (NumberFormatException nfe) {
+				return "Format is not correct for the build number parameter";
+			}
+			args = Arrays.copyOfRange(args, 0, 2);
+		}
+
+		Collection<AbstractProject<?, ?>> projects = new ArrayList<AbstractProject<?, ?>>();
+
+		final Pair<Mode, String> pair;
+		try {
+			pair = getProjects(sender, args, projects);
+		} catch (CommandException e) {
+			return getErrorReply(sender, e);
+		}
+
+		if (!projects.isEmpty()) {
+			StringBuilder msg = new StringBuilder();
+
+			switch (pair.getHead()) {
+			case SINGLE:
+				break;
+			case ALL:
+				msg.append(getCommandShortName()).append(" of all projects:\n");
+				break;
+			case VIEW:
+				msg.append(getCommandShortName()).append(" of projects in view " + pair.getTail() + ":\n");
+				break;
+			}
+
+			boolean first = true;
+			for (AbstractProject<?, ?> project : projects) {
+				if (!first) {
+					msg.append("\n");
+				} else {
+					first = false;
+				}
+
+				if (usingBuildNumber)
+					msg.append(getMessageForJobWithBuildNum(project, buildNumber));
+				else
+					msg.append(getMessageForJob(project));
+			}
+			return msg.toString();
+		} else {
+			return sender + ": no job found";
+		}
+	}
+
+	@Override
+	protected String getCommandShortName() {
+		return "changes";
+	}
+
+	/**
+	 * Return the HELP String
+	 */
 	public String getHelp() {
 		return HELP;
 	}
-	
-    public String getChangedFilePaths(ChangeLogSet<?> changeSet){
-	
-        Set<String> filePaths = new HashSet<String>();
-                for (Object o : changeSet.getItems()) {
-                	Entry entry = (Entry) o;
-        		filePaths.addAll(entry.getAffectedPaths());
-                }
+
+	private String getChangedFilePaths(ChangeLogSet<?> changeSet) {
+
+		Set<String> filePaths = new HashSet<String>();
+		for (Object o : changeSet.getItems()) {
+			Entry entry = (Entry) o;
+			filePaths.addAll(entry.getAffectedPaths());
+		}
 
 		return filePaths.toString();
-    }
+	}
 }
