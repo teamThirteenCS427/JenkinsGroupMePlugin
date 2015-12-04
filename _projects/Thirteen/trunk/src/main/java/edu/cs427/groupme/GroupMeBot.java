1fe7d463c3e866671c1b447c145cbbfd85d12134
zavelev2 <zavelev2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-04 22:51:39 +0000

    zavelev2 pzhao12 formatted GroupMeBot

--- _projects/Thirteen/trunk/src/main/java/edu/cs427/groupme/GroupMeBot.java
+++ _projects/Thirteen/trunk/src/main/java/edu/cs427/groupme/GroupMeBot.java
@@ -6,106 +6,117 @@ import org.json.JSONException;
 import org.json.simple.JSONObject;
 
 import hudson.plugins.im.bot.Bot;
- 
-/**  
- * This version is a simple implementation of the groupme bot which can send messages.
- * This version needs json library. We didn't modify the pom.xml probably need to fix the dependency issue.
+
+/**
+ * This version is a simple implementation of the groupme bot which can send
+ * messages. This version needs json library. We didn't modify the pom.xml
+ * probably need to fix the dependency issue.
+ * 
  * @author blessin2 admathu2
  * @edits pzhao12 fricken2
  *
- */ 
+ */
 public final class GroupMeBot {
-	//Base URL used for all GroupMe interactions
+	// Base URL used for all GroupMe interactions
 	public static String botName;
 	public static String accessToken;
 	public static String groupId;
 	public static String botId;
-	public static IGroupMeBotConnection connection; 
-	
+	public static IGroupMeBotConnection connection;
+
 	private static final Logger LOGGER = Logger.getLogger(GroupMeBot.class.getName());
-	
+
 	/**
 	 * getter of the bot name
+	 * 
 	 * @return name of the bot
 	 */
 	public static String getBotName() {
 		return botName;
 	}
-	
+
 	/**
 	 * initialize the GroupMeBot without botID
-	 * @param botName name of the bot
-	 * @param accessToken accessToken for GroupMe
-	 * @param groupId GroupMe group ID
-	 * @param connection IGroupMeBotConnection needed for communication
+	 * 
+	 * @param botName
+	 *            name of the bot
+	 * @param accessToken
+	 *            accessToken for GroupMe
+	 * @param groupId
+	 *            GroupMe group ID
+	 * @param connection
+	 *            IGroupMeBotConnection needed for communication
 	 */
-	public static void init(String botName, String accessToken, String groupId, IGroupMeBotConnection connection) 
-	{
+	public static void init(String botName, String accessToken, String groupId, IGroupMeBotConnection connection) {
 		GroupMeBot.botName = botName;
 		GroupMeBot.accessToken = accessToken;
 		GroupMeBot.groupId = groupId;
 		GroupMeBot.connection = connection;
 		botId = "";
 	}
-	
+
 	/**
 	 * initialize the GroupMeBot with botID
-	 * @param botId GroupMe Bot Id
-	 * @param botName name of the bot
-	 * @param accessToken accessToken for GroupMe
-	 * @param groupId GroupMe group ID
-	 * @param connection IGroupMeBotConnection needed for communication
+	 * 
+	 * @param botId
+	 *            GroupMe Bot Id
+	 * @param botName
+	 *            name of the bot
+	 * @param accessToken
+	 *            accessToken for GroupMe
+	 * @param groupId
+	 *            GroupMe group ID
+	 * @param connection
+	 *            IGroupMeBotConnection needed for communication
 	 */
-	public static void initWithBotId(String botId, String botName, String accessToken, String groupId, IGroupMeBotConnection connection)
-	{
+	public static void initWithBotId(String botId, String botName, String accessToken, String groupId,
+			IGroupMeBotConnection connection) {
 		GroupMeBot.connection = connection;
 		GroupMeBot.botId = botId;
 		GroupMeBot.botName = botName;
 		GroupMeBot.accessToken = accessToken;
 		GroupMeBot.groupId = groupId;
 	}
-	
+
 	/**
-	 * register the GroupMe Bot 
-	 * @return true if bot regiestered successfully, false if the returned JSON by the connection is null
+	 * register the GroupMe Bot
+	 * 
+	 * @return true if bot regiestered successfully, false if the returned JSON
+	 *         by the connection is null
 	 */
-	public static boolean register()
-	{
+	public static boolean register() {
 		JSONObject obj = connection.register(botName, groupId, accessToken);
-		
-		if(obj==null)
+
+		if (obj == null)
 			return false;
-		
+
 		extractBotId(obj);
 		GroupMeStoredData.setGroupMeBotId(botId);
 		return true;
 	}
-	
+
 	/**
 	 * extract bot Id from the JSONObject returned by GroupMe
-	 * @param obj JSONobject returned by the GroupeMe
+	 * 
+	 * @param obj
+	 *            JSONobject returned by the GroupeMe
 	 */
 	public static void extractBotId(JSONObject obj) {
-		if (obj == null){
+		if (obj == null) {
 			botId = "";
 			return;
 		}
 		String botIdString = "";
-		try
-		{
-			if (obj.containsKey("response"))
-			{
+		try {
+			if (obj.containsKey("response")) {
 				JSONObject response_obj = (JSONObject) obj.get("response");
-				if (response_obj.containsKey("bot"))
-				{
+				if (response_obj.containsKey("bot")) {
 					JSONObject bot_obj = (JSONObject) response_obj.get("bot");
 					if (bot_obj.containsKey("bot_id"))
 						botIdString = (String) bot_obj.get("bot_id");
 				}
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("error reading object  \n" + obj.toJSONString());
 			botIdString = "";
 		}
@@ -114,17 +125,19 @@ public final class GroupMeBot {
 
 	/**
 	 * send text message to the GroupMe group
-	 * @param message message to be sent
-	 * @return true if succeeded, false if not  
+	 * 
+	 * @param message
+	 *            message to be sent
+	 * @return true if succeeded, false if not
 	 */
-	public static boolean sendTextMessage(String message)
-	{
-		LOGGER.warning("GROUP ME BOT:: TOKEN: " + accessToken + "\n groupId: " + groupId + "\n botName: " + botName + "\n botId: "+ botId);
+	public static boolean sendTextMessage(String message) {
+		LOGGER.warning("GROUP ME BOT:: TOKEN: " + accessToken + "\n groupId: " + groupId + "\n botName: " + botName
+				+ "\n botId: " + botId);
 
 		int resp = connection.sendMessage(botId, message);
-		if (resp == 201){
+		if (resp == 201) {
 			return true;
-		} else if (resp == 420){
+		} else if (resp == 420) {
 			try {
 				Thread.sleep(60000);
 				connection.sendMessage(botId, "We need to chill, we are being rate limited");
@@ -132,17 +145,17 @@ public final class GroupMeBot {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 		return false;
 	}
-	
+
 	/**
 	 * check if the GroupeMeBot is not registered on the GroupMe
+	 * 
 	 * @return true if the bot is not registered, false if alreadly registered
 	 */
-	public static boolean isUnregistered()
-	{
+	public static boolean isUnregistered() {
 		if (botId == null)
 			botId = "";
 		return botId.equals("");
