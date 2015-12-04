a8a312c96c2ad610933b325ab69ac73fc44fb946
zavelev2 <zavelev2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-04 23:04:09 +0000

    zavelev2 pzhao12 formatted MockGroupMeBotConnection

--- src/main/java/edu/cs427/groupme/MockGroupMeBotConnection.java
+++ src/main/java/edu/cs427/groupme/MockGroupMeBotConnection.java
@@ -3,6 +3,7 @@ package edu.cs427.groupme;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 /**
  * mock GroupMeBotConnection for test use
  */
@@ -14,25 +15,26 @@ public class MockGroupMeBotConnection implements IGroupMeBotConnection {
 	public static String TEST_INVALID_TOKEN = "invalid";
 	public static String TEST_VALID_BOTID = "vBot";
 	public static String TEST_INVALID_BOTID = "iBot";
-	
+
 	/**
-     * {@inheritDoc}
-     */
+	 * {@inheritDoc}
+	 */
 	@Override
 	public JSONObject register(String botName, String groupId, String accessToken) {
-		if (accessToken.equals(TEST_VALID_TOKEN))
-		{
+		if (accessToken.equals(TEST_VALID_TOKEN)) {
 			try {
-				return (JSONObject) new JSONParser().parse("{\"response\":{\"bot\":{\"bot_id\":\""+TEST_VALID_BOTID+"\"}}}");
+				return (JSONObject) new JSONParser()
+						.parse("{\"response\":{\"bot\":{\"bot_id\":\"" + TEST_VALID_BOTID + "\"}}}");
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
 		}
 		return null;
 	}
+
 	/**
-     * {@inheritDoc}
-     */
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int sendMessage(String botId, String message) {
 		if (botId.equals(""))
