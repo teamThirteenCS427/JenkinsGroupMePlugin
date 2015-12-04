13287974ef8c6e758e8690e070fc3ea8a631119d
zavelev2 <zavelev2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-04 22:51:08 +0000

    zavelev2 pzhao12 formatted API Interface

--- src/main/java/edu/cs427/groupme/GroupMeAPIInterface.java
+++ src/main/java/edu/cs427/groupme/GroupMeAPIInterface.java
@@ -16,9 +16,11 @@ import org.json.simple.parser.ParseException;
 import hudson.plugins.im.IMConnectionProvider;
 
 import javax.net.ssl.HttpsURLConnection;
+
 /**
  * API Interface which right now only does GET requests.
- * @author espaill2, zavelev2 
+ * 
+ * @author espaill2, zavelev2
  */
 public class GroupMeAPIInterface {
 	private static final String GROUPME_URL = "https://api.groupme.com/v3";
@@ -28,23 +30,25 @@ public class GroupMeAPIInterface {
 	public GroupMeAPIInterface(String token) {
 		this.GROUPME_TOKEN = token;
 	}
+
 	/**
-	 * Separated the two mostly for unit testing purposes. This one takes an already formed URL to do the GET request.
-	 * @param url - formed url object onto which the REST connection will be established
+	 * Separated the two mostly for unit testing purposes. This one takes an
+	 * already formed URL to do the GET request.
+	 * 
+	 * @param url
+	 *            - formed url object onto which the REST connection will be
+	 *            established
 	 * @return jsonObject - returns the parsed response in a JSONObject
 	 */
-	public JSONObject GET(URL url)
-	{
+	public JSONObject GET(URL url) {
 		JSONObject json = null;
-		try 
-		{
+		try {
 			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-			
+
 			conn.setRequestMethod("GET");
 			int responseCode = conn.getResponseCode();
 
-			BufferedReader in = new BufferedReader(
-			        new InputStreamReader(conn.getInputStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			String inputLine;
 			StringBuffer response = new StringBuffer();
 
@@ -55,32 +59,34 @@ public class GroupMeAPIInterface {
 
 			json = (JSONObject) (new JSONParser().parse(response.toString()));
 
-		} 
-		catch (IOException | ParseException e) 
-		{
+		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}
-		
+
 		return json;
 	}
-	
+
 	/**
-	 * This one you can send in the endpoint and url params and the get request gets made.
-	 * @param endpoint - in other words the path to the specific endpoint in the group me service
-	 * @param param - different get params to append to url
+	 * This one you can send in the endpoint and url params and the get request
+	 * gets made.
+	 * 
+	 * @param endpoint
+	 *            - in other words the path to the specific endpoint in the
+	 *            group me service
+	 * @param param
+	 *            - different get params to append to url
 	 * @return returns JSONObject of response
 	 */
-	public JSONObject GET(String endpoint, String[] param)
-	{
+	public JSONObject GET(String endpoint, String[] param) {
 		JSONObject json = null;
-		if(GROUPME_TOKEN == null || GROUPME_TOKEN ==  ""){
+		if (GROUPME_TOKEN == null || GROUPME_TOKEN == "") {
 			LOGGER.warning("Couldn't do get request. Missing GroupMe token.");
 			return json;
 		}
 		String url = GROUPME_URL + endpoint + "?token=" + GROUPME_TOKEN;
-		if(param != null) {
+		if (param != null) {
 			LOGGER.warning("param is not null, adding to get url");
-			for(int i = 0; i < param.length; i++){
+			for (int i = 0; i < param.length; i++) {
 				url += "&" + param[i];
 			}
 		}
@@ -94,61 +100,56 @@ public class GroupMeAPIInterface {
 			return json;
 		}
 	}
-	
+
 	/**
 	 * Send a POST request to GroupMe with a JSON body
 	 */
-	public JSONObject POST_BODY(String endpoint, String body)
-	{
+	public JSONObject POST_BODY(String endpoint, String body) {
 		URL myUrl;
 		try {
 			myUrl = new URL(GROUPME_URL + endpoint + "?token=" + GROUPME_TOKEN);
 			HttpsURLConnection conn = (HttpsURLConnection) myUrl.openConnection();
-	
+
 			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Length", String.valueOf(body.length())); 
+			conn.setRequestProperty("Content-Length", String.valueOf(body.length()));
 			conn.setRequestProperty("Content-Type", "application/json");
-	
-			conn.setDoOutput(true); 
-			conn.setDoInput(true); 
+
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
 			DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
 			wr.writeBytes(body);
 			wr.flush();
 			wr.close();
-	
+
 			int responseCode = conn.getResponseCode();
-			if (responseCode == 201)
-			{
+			if (responseCode == 201) {
 				BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	
+
 				String inputLine;
 				StringBuffer response = new StringBuffer();
-	
+
 				while ((inputLine = in.readLine()) != null) {
 					response.append(inputLine);
 				}
 				in.close();
 				String res = response.toString();
-				return (JSONObject)new JSONParser().parse(res);
+				return (JSONObject) new JSONParser().parse(res);
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println(e.getStackTrace());
 			return null;
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Send a POST request to GroupMe with url parameters
 	 */
-	public int POST_PARAMS(String endpoint, String params)
-	{
+	public int POST_PARAMS(String endpoint, String params) {
 		int responseCode = 0;
-		try
-		{
-			URL url = new URL(GROUPME_URL + endpoint /*+ token=" + GROUPME_TOKEN*/);
+		try {
+			URL url = new URL(
+					GROUPME_URL + endpoint /* + token=" + GROUPME_TOKEN */);
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setDoOutput(true);
 			connection.setDoInput(true);
@@ -165,12 +166,10 @@ public class GroupMeAPIInterface {
 			responseCode = connection.getResponseCode();
 			if (responseCode != 202)
 				System.out.println(responseCode + " error has occured while sending a message");
-		} catch (MalformedURLException e)
-		{
+		} catch (MalformedURLException e) {
 			System.out.println("Error occured while establishing a connection");
 			e.printStackTrace();
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			System.out.println("Error occured while sending data");
 			e.printStackTrace();
 		}
@@ -179,10 +178,11 @@ public class GroupMeAPIInterface {
 
 	/**
 	 * Returns APIInterface GroupMe Token set when instantiated
+	 * 
 	 * @return GroupMe Token
 	 */
 	public String getGROUPME_TOKEN() {
 		return GROUPME_TOKEN;
 	}
-	
+
 }
