519b0f2d3f3385289c384e08898d9b926d262ea2
zavelev2 <zavelev2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-04 22:14:48 +0000

    Commented the GroupMeChat

--- _projects/Thirteen/trunk/src/main/java/edu/cs427/groupme/GroupMeChat.java
+++ _projects/Thirteen/trunk/src/main/java/edu/cs427/groupme/GroupMeChat.java
@@ -11,44 +11,68 @@ import hudson.plugins.im.bot.Bot;
 public class GroupMeChat implements IMChat {
 	private static final Logger LOGGER = Logger.getLogger(GroupMeChat.class.getName());
 
+	/**
+	 * Empty Constructor
+	 */
 	public GroupMeChat()
 	{
 		
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void sendMessage(String message) throws IMException {
 		GroupMeBot.sendTextMessage(message);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getNickName(String senderId) {
 		return GroupMeBot.getBotName();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getIMId(String senderId) {
 		// TODO Auto-generated method stub
         return null;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean isMultiUserChat() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean isCommandsAccepted() {
 		return true;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void addMessageListener(IMMessageListener listener) {
 		// polling already does onMessage, after new messages are parsed.
 
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void removeMessageListener(IMMessageListener listener) {
 		// polling already does onMessage, after new messages are parsed.
