94b3b361cd56d279420fadf4ff636987f62ff93c
espaill2 <espaill2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-05 23:01:58 +0000

    Lock Command Test

---
+++ src/test/java/hudson/plugins/im/bot/LockCommandTest.java
@@ -0,0 +1,64 @@
+package hudson.plugins.im.bot;
+
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import edu.cs427.groupme.GroupMeAPIInterface;
+import hudson.plugins.im.IMException;
+import hudson.plugins.im.Sender;
+
+public class LockCommandTest {
+	private Bot mockedBot;
+	private Sender sender;
+	@Before
+	public void setUp() throws Exception {
+		mockedBot = Mockito.mock(Bot.class);
+		sender = Mockito.mock(Sender.class);
+	}
+	@Test
+	public void getReplyWithNoArgumentsLocksBotIndefinitely() throws IMException{
+		LockCommand lc = new LockCommand();
+		Mockito.when(mockedBot.isSleep()).thenReturn(false);
+		assertEquals(lc.getReply(mockedBot, sender, new String[1]), "Alright I am going to sleep");
+		Mockito.verify(mockedBot, Mockito.times(1)).setSleep(true);
+	}
+	
+	@Test
+	public void getReplyWithNoArgumentsLocksBotForSpecifiedTime() throws IMException{
+		LockCommand lc = new LockCommand();
+		Mockito.when(mockedBot.isSleep()).thenReturn(false);
+		String[] args = new String[2];
+		args[1] = "5";
+		assertEquals(lc.getReply(mockedBot, sender, args), "Alright I am going to sleep for 5 seconds");
+		Mockito.verify(mockedBot, Mockito.times(1)).setSleep(true);
+	}
+	
+	@Test
+	public void getReplyWithNonsenseIntegerReturnsHelp() throws IMException{
+		LockCommand lc = new LockCommand();
+		Mockito.when(mockedBot.isSleep()).thenReturn(false);
+		String[] args = new String[2];
+		args[1] = "fsadf";
+		assertEquals(lc.getReply(mockedBot, sender, args), "lock <job> [time_to_lock_in_seconds] - schedule a lock for the specificed number of seconds");
+		Mockito.verify(mockedBot, Mockito.times(0)).setSleep(true);
+	}
+	@Test
+	public void verifyCorrectCommandName(){
+		LockCommand lc = new LockCommand();
+		Collection<String> s = lc.getCommandNames();
+		assertEquals(s.size(), 1);
+		assert(s.contains("lock"));
+	}
+	
+	@Test
+	public void verifyGetHelpString(){
+		LockCommand lc = new LockCommand();
+		assertEquals(lc.getHelp(), "lock <job> [time_to_lock_in_seconds] - schedule a lock for the specificed number of seconds");
+	}
+}
+
