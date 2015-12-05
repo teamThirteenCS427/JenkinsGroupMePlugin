eaed82f23331a4c2f5cac844d92bbc47cd55d3f7
aymei2 <aymei2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-05 00:19:20 +0000

    forgot to add TeamCommand

---
+++ _projects/Thirteen/trunk/src/main/java/hudson/plugins/im/bot/TeamCommand.java
@@ -0,0 +1,153 @@
+package hudson.plugins.im.bot;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import java.util.logging.Logger;
+
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.plugins.im.Sender;
+import hudson.plugins.im.bot.AbstractMultipleJobCommand.Mode;
+import hudson.plugins.im.tools.Pair;
+import hudson.scm.ChangeLogSet;
+import hudson.scm.ChangeLogSet.AffectedFile;
+import hudson.scm.ChangeLogSet.Entry;
+
+
+/**
+ * Returns a represntation of who has committed recently
+ * @author fricken2 aymei2
+ */
+@Extension
+public class TeamCommand extends AbstractMultipleJobCommand {
+
+    private static final Logger LOGGER = Logger.getLogger(LogCommand.class.getName());
+    private static final String HELP = "team <job> <int>- Displays a count of the most recent (2nd parameter) commits by person for said job";
+	
+
+    @Override
+    public Collection<String> getCommandNames() {
+        return Collections.singleton("team");
+    }
+
+    @Override
+    protected String getCommandShortName() {
+        return "team";
+    }
+
+    /*
+    * Returns string to display when viewing help
+    */
+    @Override
+	public String getHelp() {
+		return HELP;
+	}
+
+
+	/*
+	 * Called when someone tried to see a log via !team jobname
+	 */
+
+	@Override
+	public String getReply(Bot bot, Sender sender, String[] args) {
+    	int numBuilds = -1;
+    	boolean usingBuildNumber = false;
+    	if(args.length >= 3) {
+    		usingBuildNumber = true;
+    		try {
+    			numBuilds = Integer.parseInt(args[2]);
+    		}
+    		catch (NumberFormatException nfe) {
+    			numBuilds = 3;
+    		}
+    		args = Arrays.copyOfRange(args, 0, 2); 
+		}
+		
+		if(numBuilds >= 50 || numBuilds <= 0)
+			numBuilds = 50;
+			
+	
+		StringBuilder msg = new StringBuilder();
+
+        Collection<AbstractProject<?, ?>> projects = new ArrayList<AbstractProject<?, ?>>();
+
+        try {
+		getProjects(sender, args, projects);
+        }
+	catch (CommandException e) {
+		return getErrorReply(sender, e);
+        }
+		
+	//if there is a project with that name
+        if (!projects.isEmpty()) {
+		//For each project with that name
+            	for (AbstractProject<?, ?> project : projects) {
+                	msg.append(getCommits(project, numBuilds));
+            	}
+            	return msg.toString();
+        }
+	else {
+            return sender + ": no job found";
+        }
+	}
+	/*
+	 * For the last build, return logs by calling getChanges.
+	 */
+    	protected CharSequence getCommits(AbstractProject<?, ?> project, int numBuilds) {
+    	StringBuilder msg = new StringBuilder(32);
+        msg.append(project.getFullDisplayName() + "\n");
+
+		//Get the last build
+        AbstractBuild<?, ?> lastBuild = project.getLastBuild();
+        while(numBuilds > 0) {
+			//Get data from last build
+			if(lastBuild == null) {
+				LOGGER.warning("lastBuild was null.");
+				return "lastBuild was null.";
+			}
+			if(!lastBuild.isBuilding())
+			{
+				String changes = getChanges(lastBuild);
+				msg.append("\n" + changes);
+				msg.append("-------------");
+				numBuilds -= 1;
+			}
+			lastBuild = lastBuild.getPreviousBuild();
+		}
+		
+		return msg;
+    }
+	/*
+	 * Returns commits for a particular build.
+	 * todo when we call this function it breaks job finding for all commands
+	 */
+	public String getChanges(AbstractBuild<?, ?> r) {
+		ChangeLogSet<?> commits = r.getChangeSet();
+		//return "getChangesCalled";
+		if(commits.isEmptySet()) {
+			return "No changes this build.\n";
+		}
+		
+		String message = "";
+		for (Object o : commits.getItems()) {
+			Entry commit = (Entry) o;
+			message = "Author:" + commit.getAuthor().getDisplayName() + "\nMessage:" + commit.getMsg() + "\n";
+		}
+
+		return message;
+	}
+	
+    @Override
+    protected CharSequence getMessageForJob(AbstractProject<?, ?> project) {
+		StringBuilder msg = new StringBuilder();
+		msg.append("getMessageForJob");
+		return msg;
+    }
+}
+
