fe1770522320ddd6f1965d14dcc4789c81c6fb8c
hlee145 <hlee145@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-06 20:36:01 +0000

    hlee145 blessin2 TEST - adding test cases for GroupMeJellyData

---
+++ _projects/Thirteen/trunk/src/test/java/edu/cs427/groupme/GroupMeJellyDataTest.java
@@ -0,0 +1,56 @@
+package edu.cs427.groupme;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import edu.cs427.groupme.GroupMeJellyData.StoredData;
+import edu.cs427.groupme.GroupMeJellyData.StoredDataDescriptor;
+
+public class GroupMeJellyDataTest {
+	
+	@Test
+	public void test_StoredDataConstructor(){
+		StoredData data = new StoredData("1", "2", "3", "4", "5");
+		String bot_id = data.getGroupMeGroupId();
+		String bot_name = data.getGroupMeBotName();
+		String token = data.getGroupMeToken();
+		String group_name = data.getGroupMeGroupName();
+		String cmd_prefix = data.getBotCommandPrefix();
+		assertEquals("1", bot_id);
+		assertEquals("2", token);
+		assertEquals("3", bot_name);
+		assertEquals("4", group_name);
+		assertEquals("5", cmd_prefix);
+	}
+	
+	@Test
+	public void test_StoredDatatoString(){
+		StoredData data = new StoredData("1", "2", "3", "4", "5");
+		String expected = "[1, 2, 3, 4, 5]";
+		String actual = data.toString();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_StoredDatatoString_nullcheck(){
+		StoredData data = new StoredData(null, null, null, null, null);
+		String expected = "[null, null, null, null, null]";
+		String actual = data.toString();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_StoredDataDescriptor(){
+		StoredData data = new StoredData("1", "2", "3", "4", "5");
+		StoredDataDescriptor d = (StoredDataDescriptor) data.getDescriptor();
+		assertNotNull(d);
+		String expected_classname = "StoredData";
+		String actual_classname = d.getDisplayName();
+		assertEquals(expected_classname, actual_classname);
+	}
+}
