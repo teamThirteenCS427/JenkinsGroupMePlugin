d3a62e2d571a4c21901e89c54e72ce698c2c87a8
hlee145 <hlee145@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-06 21:32:54 +0000

    hlee145 blessin2 Fixing a few errors

--- src/test/java/edu/cs427/groupme/GroupMeJellyDataTest.java
+++ src/test/java/edu/cs427/groupme/GroupMeJellyDataTest.java
@@ -1,15 +1,10 @@
 package edu.cs427.groupme;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 import edu.cs427.groupme.GroupMeJellyData.StoredData;
-import edu.cs427.groupme.GroupMeJellyData.StoredDataDescriptor;
 
 public class GroupMeJellyDataTest {
 	
