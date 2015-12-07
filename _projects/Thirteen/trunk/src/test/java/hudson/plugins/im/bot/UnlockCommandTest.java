d4995c53840325a0b7f2a9f8c27db00ba9381c2c
espaill2 <espaill2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-07 02:41:20 +0000

    Modified unlock command test, and added two more edge cases

--- _projects/Thirteen/trunk/src/test/java/hudson/plugins/im/bot/UnlockCommandTest.java
+++ _projects/Thirteen/trunk/src/test/java/hudson/plugins/im/bot/UnlockCommandTest.java
@@ -8,24 +8,54 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import edu.cs427.groupme.GroupMeStoredData;
 import hudson.plugins.im.IMException;
 import hudson.plugins.im.Sender;
 
 public class UnlockCommandTest {
 	private Bot mockedBot;
 	private Sender sender;
+	private String nickname;
 	@Before
 	public void setUp() throws Exception {
 		mockedBot = Mockito.mock(Bot.class);
 		sender = Mockito.mock(Sender.class);
 	}
 	@Test
-	public void getReplyWhenBotisAsleep() throws IMException{
+	public void getReplyWhenBotisAsleepAndCorrectUser() throws IMException{
+		nickname = "darko";
+		Mockito.when(sender.getNickname()).thenReturn(nickname);
+		GroupMeStoredData.setLockedByUsername(nickname);
 		UnlockCommand ul = new UnlockCommand();
 		Mockito.when(mockedBot.isSleep()).thenReturn(true);
 		assertEquals("Going back to work...", ul.getReply(mockedBot, sender, new String[1]));
 		Mockito.verify(mockedBot, Mockito.times(1)).setSleep(false);
 	}
+	
+	@Test
+	public void getReplyWhenBotisNotAsleepAndCorrectUser() throws IMException{
+		nickname = "darko";
+		Mockito.when(sender.getNickname()).thenReturn(nickname);
+		GroupMeStoredData.setLockedByUsername(nickname);
+		UnlockCommand ul = new UnlockCommand();
+		Mockito.when(mockedBot.isSleep()).thenReturn(false);
+		assertEquals("I was never locked", ul.getReply(mockedBot, sender, new String[1]));
+		Mockito.verify(mockedBot, Mockito.times(0)).setSleep(false);
+	}
+	
+	@Test
+	public void getReplyWhenBotisAsleepAndIncorrectUser() throws IMException{
+		nickname = "darko";
+		String unauthorizedNickname = "semih";
+		String properResponse = "Hey what do you think you are doing! " + unauthorizedNickname + " locked the chat.";
+		Mockito.when(sender.getNickname()).thenReturn(nickname);
+		GroupMeStoredData.setLockedByUsername(unauthorizedNickname);
+		UnlockCommand ul = new UnlockCommand();
+		Mockito.when(mockedBot.isSleep()).thenReturn(true);
+		assertEquals(properResponse, ul.getReply(mockedBot, sender, new String[1]));
+		Mockito.verify(mockedBot, Mockito.times(0)).setSleep(false);
+	}
+	
 	
 	@Test
 	public void getReplyWhenBotisAlreadyAwake() throws IMException{
@@ -44,6 +74,8 @@ public class UnlockCommandTest {
 	
 	@Test
 	public void verifyGetHelpString(){
+		Mockito.when(sender.getNickname()).thenReturn(nickname);
+		GroupMeStoredData.setLockedByUsername(nickname);
 		UnlockCommand ul = new UnlockCommand();
 		assertEquals("unlock", ul.getHelp());
 	}
