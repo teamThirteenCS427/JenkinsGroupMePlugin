f5824c851a5e88bddb6d683fee3bd2baf2aeaaa0
admathu2 <admathu2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-09 00:09:33 +0000

    removed unwanted imports in test/im

--- src/test/java/hudson/plugins/im/NotificationStrategyTest.java
+++ src/test/java/hudson/plugins/im/NotificationStrategyTest.java
@@ -1,9 +1,5 @@
 package hudson.plugins.im;
 
-import hudson.model.AbstractBuild;
-import hudson.model.Result;
-import org.junit.Test;
-
 import static hudson.model.Result.ABORTED;
 import static hudson.model.Result.FAILURE;
 import static hudson.model.Result.NOT_BUILT;
@@ -12,6 +8,11 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+
+import hudson.model.AbstractBuild;
+import hudson.model.Result;
 
 public class NotificationStrategyTest {
 
